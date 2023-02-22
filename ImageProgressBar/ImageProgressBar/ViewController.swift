7c65a9441f11b4110cb67dcddf926b4faaab8c2b
Young June Park <pyj9748@naver.com>
2023-02-22 16:46:42 +0900

    [chore] remove StoryBoard

---
+++ ImageProgressBar/ImageProgressBar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageProgressBar
+//
+//  Created by young june Park on 2023/02/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
